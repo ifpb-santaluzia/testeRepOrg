3ea5ba8150c904ef4d00e46ba7886f7155378153
Pedro <pedro-brito.pb@academico.ifpb.edu.br>
2024-05-08 11:32:02 -0300

    Criação da classe "Conta"

---
+++ app/src/main/java/com/example/testereporg/Conta.java
@@ -0,0 +1,27 @@
+package com.example.testereporg;
+
+public class Conta {
+    private int numero;
+    private int agencia;
+
+    public Conta(int numero, int agencia) {
+        this.numero = numero;
+        this.agencia = agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+}
