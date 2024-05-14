6d31172c302d3d1f204301688178f6229a91a6c7
alexandrecostapbGIT <costa.alexandre@ifpb.edu.br>
2024-05-14 11:34:38 -0300

    teste

--- app/src/main/java/com/example/testereporg/MainActivity.java
+++ app/src/main/java/com/example/testereporg/MainActivity.java
@@ -20,6 +20,6 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        System.out.println("testse");
+        System.out.println("test");
     }
 }
